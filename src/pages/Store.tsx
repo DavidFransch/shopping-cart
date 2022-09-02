df3a863a3627d5a8bfce061d758085b853c99557
DavidFransch <djsfdavid@gmail.com>
2022-09-02 16:07:45 +0200

    feat: setup routing and static ui

---
+++ src/pages/Store.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
+import { StoreItem } from '../components/StoreItem'
+
+export const Store = () => {
+  return (
+    <>
+      <div>Store</div>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col key={item.id}><StoreItem {... item} /></Col>
+        ))}
+      </Row>
+    </>
+  )
+}
