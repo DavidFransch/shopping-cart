7cc7c58a22aa8be22c42fe9f3b7b9dbfa46e2d33
DavidFransch <djsfdavid@gmail.com>
2022-09-05 20:06:33 +0200

    feat: create and add state context

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,82 @@
+import { useContext, createContext, ReactNode, useState } from 'react'
+
+interface ShoppingCartContext {
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeFromCart: (id: number) => void
+}
+
+interface ShoppingCartProviderProps {
+  children: ReactNode
+}
+
+interface CartItem {
+  id: number
+  quantity: number
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.quantity || 0
+  }
+
+  const increaseCartQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }]
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 }
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  const decreaseCartQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id)
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 }
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  const removeFromCart = (id: number) => {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id)
+    })
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  )
+}
